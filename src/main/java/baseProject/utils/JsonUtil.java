f22f4d816ade89232b081e5c861aca70914e2f65
caoye <mailcaoye@163.com>
2018-05-29 15:38:20 +0800

    add jsonutil logger

---
+++ src/main/java/baseProject/utils/JsonUtil.java
@@ -0,0 +1,84 @@
+package baseProject.utils;
+
+import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class JsonUtil {
+	private static Logger logger = LogUtil.getLogger();
+	private static ObjectMapper mapper = new ObjectMapper();
+	private static XmlMapper xmlMapper = new XmlMapper();
+
+	static {
+		mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		mapper.setSerializationInclusion(Include.NON_NULL);
+		xmlMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		xmlMapper.setSerializationInclusion(Include.NON_NULL);
+	}
+
+	/**
+	 * 对象转json
+	 * 
+	 * @param jsonString
+	 * @param c
+	 * @return
+	 */
+	public static Object getObjectFromJson(String jsonString, Class<?> c) {
+		try {
+			return mapper.readValue(jsonString, c);
+		} catch (Exception e) {
+			logger.info("Get Object From Json Error:" + e.toString());
+			return null;
+		}
+	}
+
+	/**
+	 * 将对象转为JSON字符串
+	 * 
+	 * @param object
+	 * @return
+	 */
+	public static String getJsonFromObject(Object object) {
+		String jsonString;
+		// 美化输出，仅调试使用
+		// mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		// 允许输出空对象
+
+		try {
+			jsonString = mapper.writeValueAsString(object);
+		} catch (JsonProcessingException e) {
+			jsonString = null;
+			logger.warn(e.toString());
+		}
+		return jsonString;
+	}
+
+	/**
+	 * 将对象转换为XML字符串
+	 * 
+	 * @param object
+	 * @return
+	 */
+	public static String getXMLFromObject(Object object) {
+		String xmlString;
+		try {
+			xmlString = xmlMapper.writeValueAsString(object);
+		} catch (Exception e) {
+			xmlString = null;
+		}
+		return xmlString;
+	}
+
+	public static Object getObjectFromXML(String XMLString, Class<?> c) {
+		try {
+			return xmlMapper.convertValue(XMLString, c);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
---
+++ src/main/java/baseProject/utils/LogUtil.java
@@ -0,0 +1,38 @@
+package baseProject.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogUtil {
+	private static Logger logger = LogManager.getLogger(LogUtil.class);
+	private static Logger exceptionLogger = LogManager.getLogger(LogUtil.class);
+
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	public static void info(String msg) {
+		logger.info(msg);
+	}
+
+	public static void info(String msg, Exception e) {
+		logger.info(msg, e);
+	}
+
+	public static void warn(String msg) {
+		logger.warn(msg);
+	}
+
+	public static void warn(String msg, Exception e) {
+		logger.warn(msg, e);
+	}
+
+	public static void error(String msg) {
+		logger.error(msg);
+	}
+
+	public static void error(String msg, Exception e) {
+		logger.error(msg, e);
+	}
+
+}
