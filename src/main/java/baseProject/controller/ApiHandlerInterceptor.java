0a7b4a0e8c6dd20117be0bdae95280219371691e
caoye <mailcaoye@163.com>
2018-06-01 19:01:14 +0800

    tmp

--- src/main/java/baseProject/controller/ApiHandlerInterceptor.java
+++ src/main/java/baseProject/controller/ApiHandlerInterceptor.java
@@ -3,13 +3,20 @@ package baseProject.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import baseProject.utils.LogUtil;
 
 public class ApiHandlerInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		System.out.println("get request");
+		request.getParameterMap().forEach((k,v)->{
+			LogUtil.debug("request params key:"+k+" value:"+v);
+		});
+		LogUtil.debug(request.getMethod());
+		LogUtil.debug(request.getRequestURL().toString());
 		return true;
 	}
 
